ddb3f7215a92007c2d8e5545566566c8f2e7d092
Bhavya Jain <contact.jain.bhavya@gmail.com>
2017-12-14 09:35:10 +0100

    Updated Manage Servers and Nodes
    Removed certain errors.

--- src/Nodes.java
+++ src/Nodes.java
@@ -13,12 +13,55 @@ public class Nodes extends UnicastRemoteObject implements Nodes_Interface, Runna
 	
 	private static final long serialVersionUID = 4745507L;
 	
+	/*
+	 * For logging
+	 */
+	
 	private static Logger logger = Logger.getLogger(Nodes.class.getName());
 	
+	/*
+	 * To store url of server
+	 */
+	
+	private Map<String, Nodes_Interface> serverDetails;
+	
+	/*
+	 * Index number of the server
+	 */
+	
+	private int serverIndex;
+	
+	/*
+	 * Return serverIndex
+	 * Debugging
+	 * Private Check: 0000 0000
+	 */
+	
+	public int getServerIndex() {
+		return this.serverIndex;
+	}
+	
+	/*
+	 * Default Constructor
+	 */
 	
 	protected Nodes(int totalServers, int serverIndex) throws RemoteException{
 		super();
 	}
+	
+	/*
+	 * Reset Server
+	 */
+	
+	
+	public void reset() {
+		
+	}
+	
+	/*
+	 * Server start Logger
+	 */
+	
 	
 	public void run() {
 		logger.info("Server successfully initialized and started");
--- src/Nodes_Interface.java
+++ src/Nodes_Interface.java
@@ -1,4 +1,26 @@
 
-public interface Nodes_Interface {
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+import java.util.List;
+import java.util.Map;
+
+
+public interface Nodes_Interface extends Remote{
+	
+	/*
+	 * Return serverIndex
+	 * Debugging
+	 * Private Check: 0000 0000
+	 */
+	
+	public int getServerIndex() throws RemoteException;
+	
+	/*
+	 * Reset Server
+	 */
+	
+	
+	public void reset() throws RemoteException;
 
 }
