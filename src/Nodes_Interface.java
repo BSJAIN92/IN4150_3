f5a7b423a80b082e0c8e2987e62e51d34854fa16
Gothik32 <33715563+Gothik32@users.noreply.github.com>
2017-12-14 10:38:55 +0100

    Update Nodes_Interface.java

--- src/Nodes_Interface.java
+++ src/Nodes_Interface.java
@@ -1,4 +1,7 @@
+import com.sun.javafx.geom.Edge;
 
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
@@ -6,21 +9,54 @@ import java.util.List;
 import java.util.Map;
 
 
-public interface Nodes_Interface extends Remote{
-	
+    public interface Nodes_Interface extends Remote {
+
 	/*
 	 * Return serverIndex
 	 * Debugging
 	 * Private Check: 0000 0000
 	 */
-	
-	public int getServerIndex() throws RemoteException;
-	
+
+        public int getServerIndex() throws RemoteException;
+
 	/*
 	 * Reset Server
 	 */
-	
-	
-	public void reset() throws RemoteException;
 
-}
+
+        public void reset() throws RemoteException;
+
+        public void setNeighbourEdges() throws RemoteException;
+
+        public List<Edge> getNeighbourEdges() throws RemoteException;
+
+        public void setFragmentLevel(int fragmentLevel) throws RemoteException;
+
+        public int getFragmentLevel() throws RemoteException;
+
+        public void setFragmentName(String fragmentName)throws RemoteException;
+
+        public String getFragmentName() throws RemoteException;
+
+        public void setCoreOfFragment(boolean coreOfFragment) throws RemoteException;
+
+        public boolean getCoreOfFragment() throws RemoteException;
+
+        public void setStatus(String status) throws RemoteException;
+
+        public String getStatus() throws RemoteException;
+
+        public void setMoeCandidate(Edge moeCandidate) throws RemoteException;
+
+        public Edge getMoeCandidate() throws RemoteException;
+
+        public void setnumberReportMessagesExpected(int numberReportMessagesExpected) throws RemoteException;
+
+        public int getnumberReportMessagesExpected() throws RemoteException;
+
+        public void setStatusOfEdge(Map<Edge, String> statusOfEdge) throws RemoteException;
+
+        public Map<Edge, String> getStatusOfEdge() throws RemoteException;
+    }
+
+
